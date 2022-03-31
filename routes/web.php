1a649b069e611d5b7348ccf9c7b6fd3c05b8134f
Mouad Louhibi <mouadlouhibi2000@gmail.com>
2022-03-31 01:49:34 +0000

    Jetstream

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,13 @@ Route::post('/add/post', 'HomeController@store')->name('post.store');
 Route::get('/edit/post/{slug}', 'HomeController@edit')->name('post.edit');
 Route::put('/update/post/{slug}', 'HomeController@update')->name('post.update');
 Route::delete('/delete/post/{slug}', 'HomeController@delete')->name('post.delete');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
