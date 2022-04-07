1b8501f2428b753667a6a3f5feb8be7e316c3cbf
Mouad Louhibi <mouadlouhibi2000@gmail.com>
2022-04-07 02:21:07 +0000

    Routes

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::middleware([
 
 Route::resource('categories', 'CategoryController');
 Route::resource('posts', 'PostController');
+Route::get('/post/delete/{slug}', 'PostController@delete')->name('posts.delete');
+Route::get('/post/restore/{slug}', 'PostController@restore')->name('posts.restore');
